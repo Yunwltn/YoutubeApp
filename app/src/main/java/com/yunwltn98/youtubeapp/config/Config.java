dd5bb686190e5c4e6f28534fe0114fa1fedd02d7
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2023-04-03 11:02:17 +0900

    ...

--- app/src/main/java/com/yunwltn98/youtubeapp/config/Config.java
+++
@@ -1,9 +0,0 @@
-package com.yunwltn98.youtubeapp.config;
-
-public class Config {
-
-    public static final String BASE_URL = "https://www.googleapis.com";
-    public static final String PATH = "/youtube/v3/search";
-    public static final String API_KEY = "AIzaSyCUsdi8yAclDt6LiyZ1k7EA5f6Yr7YLSYQ";
-
-}
